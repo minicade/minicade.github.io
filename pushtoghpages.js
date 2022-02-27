2d4ec5ccd76434b708b484bda239df57ede55bf5
ewsgit <ewsgit@outlook.com>
2022-02-27 18:09:39 +0000

    1

--- pushtoghpages.js
+++ pushtoghpages.js
@@ -2,5 +2,5 @@ const ghpages = require("gh-pages");
 
 ghpages.publish("src", {
   branch: "gh-pages",
-  repo: "https://github.com/ewsgit/devdash.git",
+  repo: "https://github.com/minicade/minicade.github.io.git",
 });

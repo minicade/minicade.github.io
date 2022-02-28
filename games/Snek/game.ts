595a54ad81e8579b1630ccf3c3b35edf48d7dd9a
ewsgit <ewsgit@outlook.com>
2022-02-28 17:25:07 +0000

    Updates

---
+++ games/Snek/game.ts
@@ -0,0 +1,214 @@
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const START_LENGTH = 5;
+const STARTPOS = { x: -4, y: 0 };
+const STARTAPPLEPOS = { x: -3, y: 0 };
+
+class Game {
+  gridScale!: number;
+  x!: number;
+  y!: number;
+  trail!: Array<{ x: number; y: number }>;
+  direction: string = "right";
+  gridSize!: number;
+  score: number = 0;
+  applePos!: Array<{ x: number; y: number }>;
+  currentSubTick: number = 0;
+  constructor(firstRun: boolean) {
+    this.start(firstRun);
+  }
+  start(firstRun: boolean) {
+    this.x = 0;
+    this.y = 0;
+    this.trail = [];
+    this.direction = "right";
+    this.gridSize = 50;
+    this.gridScale = canvas.width / this.gridSize;
+    this.score = 0;
+    this.applePos = [];
+    if (firstRun === true) {
+      this.render();
+      setInterval(this.handleSubTick.bind(this), 10);
+    }
+  }
+  handleSubTick() {
+    this.currentSubTick++;
+    if (this.currentSubTick === 10) {
+      this.currentSubTick = 0;
+      this.tick();
+    }
+  }
+  reset() {
+    this.start(false);
+  }
+  isSnakeOutOfGameWalls() {
+    if (
+      this.x < 0 ||
+      this.x > this.gridSize - 1 ||
+      this.y < 0 ||
+      this.y > this.gridSize - 1
+    ) {
+      return true;
+    }
+    return false;
+  }
+  render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#008800";
+    ctx.fillRect(
+      0,
+      0,
+      this.gridSize * this.gridScale,
+      this.gridSize * this.gridScale
+    );
+    for (let i = 0; i < this.gridSize; i++) {
+      // draw grid
+      ctx.fillStyle = "#00000033";
+      ctx.lineWidth = 0.25;
+      ctx.beginPath();
+      ctx.moveTo(i * this.gridScale, 0);
+      ctx.lineTo(i * this.gridScale, this.gridSize * this.gridScale);
+      ctx.moveTo(0, i * this.gridScale);
+      ctx.lineTo(this.gridSize * this.gridScale, i * this.gridScale);
+      ctx.stroke();
+    }
+    // draw snake
+    ctx.fillStyle = "red";
+    for (let i = 0; i < this.trail.length; i++) {
+      ctx.fillRect(
+        this.trail[i].x * this.gridScale,
+        this.trail[i].y * this.gridScale,
+        this.gridScale,
+        this.gridScale
+      );
+    }
+    // draw snake head
+    ctx.fillStyle = "black";
+    switch (this.direction) {
+      case "right":
+        ctx.fillRect(
+          this.x * this.gridScale + (this.currentSubTick * this.gridScale) / 10,
+          this.y * this.gridScale,
+          this.gridScale,
+          this.gridScale
+        );
+        ctx.fillRect(
+          this.x * this.gridScale +
+            (this.currentSubTick * this.gridScale) / 10 +
+            this.gridScale / 2,
+          this.y * this.gridScale + this.gridScale / 2 - this.gridScale / 20,
+          this.gridScale,
+          this.gridScale / 10
+        );
+        break;
+      case "left":
+        ctx.fillRect(
+          this.x * this.gridScale - (this.currentSubTick * this.gridScale) / 10,
+          this.y * this.gridScale,
+          this.gridScale,
+          this.gridScale
+        );
+        ctx.fillRect(
+          this.x * this.gridScale -
+            (this.currentSubTick * this.gridScale) / 10 -
+            this.gridScale / 2,
+          this.y * this.gridScale + this.gridScale / 2 - this.gridScale / 20,
+          this.gridScale,
+          this.gridScale / 10
+        );
+        break;
+      case "up":
+        ctx.fillRect(
+          this.x * this.gridScale,
+          this.y * this.gridScale - (this.currentSubTick * this.gridScale) / 10,
+          this.gridScale,
+          this.gridScale
+        );
+        ctx.fillRect(
+          this.x * this.gridScale + this.gridScale / 2 - this.gridScale / 20,
+          this.y * this.gridScale -
+            (this.currentSubTick * this.gridScale) / 10 -
+            this.gridScale / 2,
+          this.gridScale / 10,
+          this.gridScale
+        );
+        break;
+      case "down":
+        ctx.fillRect(
+          this.x * this.gridScale,
+          this.y * this.gridScale + (this.currentSubTick * this.gridScale) / 10,
+          this.gridScale,
+          this.gridScale
+        );
+        ctx.fillRect(
+          this.x * this.gridScale + this.gridScale / 2 - this.gridScale / 20,
+          this.y * this.gridScale +
+            (this.currentSubTick * this.gridScale) / 10 +
+            this.gridScale / 2,
+          this.gridScale / 10,
+          this.gridScale
+        );
+        break;
+    }
+
+    // draw apple
+    ctx.fillStyle = "yellow";
+    for (let i = 0; i < this.applePos.length; i++) {
+      ctx.fillRect(
+        this.applePos[i].x * this.gridScale,
+        this.applePos[i].y * this.gridScale,
+        this.gridScale,
+        this.gridScale
+      );
+    }
+    requestAnimationFrame(this.render.bind(this));
+  }
+  tick() {
+    switch (this.direction) {
+      case "right":
+        this.x++;
+        break;
+      case "left":
+        this.x--;
+        break;
+      case "up":
+        this.y--;
+        break;
+      case "down":
+        this.y++;
+        break;
+      default:
+        break;
+    }
+    if (this.isSnakeOutOfGameWalls()) {
+      this.reset();
+    }
+    // check if snake hit itself
+    for (let i = 0; i < this.trail.length; i++) {
+      // if snake head hits a part of its body
+      if (this.trail[i].x === this.x && this.trail[i].y === this.y) {
+        this.reset();
+      }
+    }
+  }
+}
+
+let game = new Game(true);
+
+window.addEventListener("keydown", e => {
+  switch (e.key) {
+    case "ArrowUp":
+      game.direction = "up";
+      break;
+    case "ArrowDown":
+      game.direction = "down";
+      break;
+    case "ArrowLeft":
+      game.direction = "left";
+      break;
+    case "ArrowRight":
+      game.direction = "right";
+      break;
+  }
+});
--- scripts/loadGame.ts
+++ scripts/loadGame.ts
@@ -1,6 +1,11 @@
-let doc = document.getElementById("game") as HTMLDivElement;
+let LOAD_doc = document.getElementById("game") as HTMLDivElement;
 
-let query = new URLSearchParams(window.location.search);
-let game = query.get("game");
-
-doc.innerHTML = `<iframe src="./../games/${game}/index.html" width="${doc.clientWidth}" height="${doc.clientHeight}" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation" frameborder="0"></iframe>`;
+let LOAD_query = new URLSearchParams(window.location.search);
+let LOAD_game = LOAD_query.get("game");
+if (LOAD_game !== null) {
+  LOAD_doc.innerHTML = `<iframe src="./../games/${LOAD_game}/index.html" width="${LOAD_doc.clientWidth}" height="${LOAD_doc.clientHeight}" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation" frameborder="0"></iframe>`;
+} else {
+  LOAD_doc.innerHTML = `<h1 style="font-size:3rem;">No game selected</h1>      <button onclick="window.location.href='..'" class="text-2xl bg-branding p-4 pl-8 pr-8 rounded-2xl m-0 hover:bg-branding-light active:bg-branding-dark transition-colors border-branding-light hover:border-branding hover:border-opacity-100 border-4 border-opacity-0">
+    Go Back
+  </button>`;
+}
